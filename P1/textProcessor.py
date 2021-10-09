22144863da306f0819ff31b295a0b84147567765
alejandro-macias <matiasale9@gmail.com>
2021-10-08 22:59:30 -0600

    adding a textProcessor.py file

---
+++ P1/textProcessor.py
@@ -0,0 +1,9 @@
+# **************************************************************
+#
+# This is the text processor python file which will process the text in the
+# csv file, utilize stop words, stem.
+#
+# AUTHORS: Amara Tariq, Steven Kim, Alejandro Macias
+#
+# **************************************************************
+
